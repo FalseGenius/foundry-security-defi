f81cb51d9d3d0aa8fdd9719ecfcb446499153c3e
FalseGenius <billionairejackal@gmail.com>
2024-03-12 17:47:10 +0500

    Natspec Doc

--- src/DecentralizedStableCoin.sol
+++ src/DecentralizedStableCoin.sol
@@ -2,6 +2,16 @@
 
 pragma solidity ^0.8.18;
 
+
+/**
+ * @title Decentralized Stablecoin 
+ * @author FalseGenius
+ * Collateral: Exogenous (ETH & BTC)
+ * Minting: Algorithmic
+ * Relative Stability: Pegged to USD
+ * 
+ * @notice This contract is governed by DSCEngine. This contract is ERC20 implementation of stablecoin system.
+ */
 contract DecentralizedStableCoin {
-    
+
 }
