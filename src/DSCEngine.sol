6cd9ce185fd659bcad551acf44890a1723c30b74
FalseGenius <billionairejackal@gmail.com>
2024-03-13 14:21:15 +0500

    DSCEngine contract added

---
+++ src/DSCEngine.sol
@@ -0,0 +1,13 @@
+// SPDX-License-Identifier: MIT
+
+pragma solidity ^0.8.18;
+
+
+/**
+ * @title DSCEngine
+ * @author False Genius
+ * This system is designed to be as minimal as possible, and have tokens maintain a 1 token == $1 peg.
+ */
+contract DSCEngine {
+    
+}
