3977a14a89e416728eb5b6ee448549ac087058be
FalseGenius <billionairejackal@gmail.com>
2024-03-15 17:47:10 +0500

    getOrCreateAnvilConfig Mock added

--- script/HelperConfig.s.sol
+++ script/HelperConfig.s.sol
@@ -1,9 +1,11 @@
 // SPDX-License-Identifier: UNLICENSED
 pragma solidity ^0.8.18;
 
-import {Script, console} from "forge-std/Script.sol";
 import {DSCEngine} from "../src/DSCEngine.sol";
+import {Script, console} from "forge-std/Script.sol";
+import {MockV3Aggregator} from "../test/mocks/MockV3Aggregator.sol";
 import {DecentralizedStableCoin} from "../src/DecentralizedStableCoin.sol";
+import {ERC20Mock} from "@openzeppelin/contracts/mocks/token/ERC20Mock.sol";
 
 contract HelperConfig is Script {
     struct NetworkConfig {
@@ -14,6 +16,14 @@ contract HelperConfig is Script {
         uint256 deployerKey;
     }
 
+    address public constant WETH = 0xdd13E55209Fd76AfE204dBda4007C227904f0a81;
+    address public constant WBTC = 0x8f3Cf7ad23Cd3CaDbD9735AFf958023239c6A063;
+    address public constant WETH_PRICE_FEED = 0x694AA1769357215DE4FAC081bf1f309aDC325306;
+    address public constant WBTC_PRICE_FEED = 0x1b44F3514812d835EB1BDB0acB33d3fA3351Ee43;
+
+    uint8 public constant DECIMALS = 8;
+    int256 public constant ETH_USD_PRICE = 2000e8;
+
     NetworkConfig public activeNetwork;
 
     constructor() {
@@ -21,6 +31,21 @@ contract HelperConfig is Script {
         else activeNetwork = getOrCreateAnvilConfig();
     }
 
-    function getSepoliaConfig() public returns (NetworkConfig memory) {}
-    function getOrCreateAnvilConfig() public returns (NetworkConfig memory) {}
+    function getSepoliaConfig() public view returns (NetworkConfig memory config) {
+        return NetworkConfig({
+            wethUsdPriceFeed:WETH_PRICE_FEED,
+            wbtcUsdPriceFeed:WBTC_PRICE_FEED,
+            weth:WETH,
+            wbtc:WBTC,
+            deployerKey:vm.envUint("RPRIVATE_KEY")
+        });
+    }
+    function getOrCreateAnvilConfig() public returns (NetworkConfig memory config) {
+        if (activeNetwork.wethUsdPriceFeed != address(0)) return activeNetwork;
+        vm.startBroadcast();
+        MockV3Aggregator ethPriceFeeds = new MockV3Aggregator(DECIMALS, ETH_USD_PRICE);
+        ERC20Mock wethMock = new ERC20Mock();
+        // ERC20Mock wbtcMock = new ERC20Mock("WBTC", "WBTC", msg.sender, 1000e8);
+        vm.stopBroadcast();
+    }
 }
