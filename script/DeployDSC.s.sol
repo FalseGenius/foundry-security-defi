0ffc3c3b66fdeab058b048ad8214cf5fffaa48f1
FalseGenius <billionairejackal@gmail.com>
2024-03-16 22:55:30 +0500

    tokenAddresses/priceFeedAddresses added to deploy script

--- script/DeployDSC.s.sol
+++ script/DeployDSC.s.sol
@@ -7,14 +7,23 @@ import {DecentralizedStableCoin} from "../src/DecentralizedStableCoin.sol";
 import {HelperConfig} from "../script/HelperConfig.s.sol";
 
 contract DeployDSC is Script {
+
+    address[] public tokenAddresses;
+    address[] public priceFeedAddresses;
+
     function run() external returns (DecentralizedStableCoin, DSCEngine) {
         HelperConfig config = new HelperConfig();
         (address wethUsdPriceFeed, address wbtcUsdPriceFeed, address weth, address wbtc, uint256 deployerKey) =
             config.activeNetwork();
 
+        tokenAddresses = [weth, wbtc];
+        priceFeedAddresses = [wethUsdPriceFeed, wbtcUsdPriceFeed];
+        
         vm.startBroadcast();
         DecentralizedStableCoin dsc = new DecentralizedStableCoin();
-        // DSCEngine engine = new DSCEngine(,,address(dsc))
+        DSCEngine engine = new DSCEngine(tokenAddresses, priceFeedAddresses,address(dsc));
         vm.stopBroadcast();
+
+        return (dsc, engine);
     }
 }
