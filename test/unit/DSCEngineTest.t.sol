18a33e3fce13f1bec37974a8b93ba0f36eddacff
FalseGenius <billionairejackal@gmail.com>
2024-03-21 15:23:58 +0500

    Deposit collateral test added

--- test/unit/DSCEngineTest.t.sol
+++ test/unit/DSCEngineTest.t.sol
@@ -6,6 +6,7 @@ import {Test, console} from "forge-std/Test.sol";
 import {DSCEngine} from "../../src/DSCEngine.sol";
 import {DeployDSC} from "../../script/DeployDSC.s.sol";
 import {HelperConfig} from "../../script/HelperConfig.s.sol";
+import {ERC20Mock} from "@openzeppelin/contracts/mocks/token/ERC20Mock.sol";
 import {DecentralizedStableCoin} from "../../src/DecentralizedStableCoin.sol";
 
 contract DSCEngineTest is Test {
@@ -13,6 +14,9 @@ contract DSCEngineTest is Test {
     address weth;
     address wethUsdPriceFeed;
     address public alice = makeAddr("alice");
+
+    uint256 constant AMOUNT_COLLATERAL = 10 ether;
+    uint256 constant STARTING_ERC20_BALANCE = 10 ether;
     
     DSCEngine public engine;
     DeployDSC public deployer;
@@ -23,7 +27,10 @@ contract DSCEngineTest is Test {
         deployer = new DeployDSC();
         (dsc, engine, config) = deployer.run();
         (wethUsdPriceFeed,, weth,,) = config.activeNetwork();
-        deal(alice, 1 ether);
+
+        // The lines below achieve the same. They both deal alice 10 ether worth weth.
+        ERC20Mock(weth).mint(alice, STARTING_ERC20_BALANCE);
+        // deal(weth, alice, STARTING_ERC20_BALANCE);
     }
 
 
@@ -37,5 +44,19 @@ contract DSCEngineTest is Test {
         uint256 expectedUsd = 30000e18;
         uint256 actualUsd = engine.getUsdValue(weth, ethAmount);
         assertEq(expectedUsd, actualUsd);
+    }
+
+    ////////////////////////////////
+    /// Deposit Collateral Tests ///
+    ////////////////////////////////
+
+    function testRevertsIfCollateralValueIsZero() public {
+        vm.startPrank(alice);
+        ERC20Mock(weth).approve(address(engine), AMOUNT_COLLATERAL);
+        
+        vm.expectRevert(DSCEngine.DSCEngine__NeedsMoreThanZero.selector);
+        engine.depositCollateral(weth, 0);
+        vm.stopPrank();
+
     }
 }
