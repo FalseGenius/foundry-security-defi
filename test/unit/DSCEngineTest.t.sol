569f9ce4f8f654b09981abdfa19aafdb0df94568
FalseGenius <billionairejackal@gmail.com>
2024-03-21 12:58:23 +0500

    DSCEngineTest added

---
+++ test/unit/DSCEngineTest.t.sol
@@ -0,0 +1,15 @@
+// SPDX-License-Identifier: MIT
+
+pragma solidity ^0.8.18;
+
+import {Test} from "forge-std/Test.sol";
+import {HelperConfig} from "../../script/HelperConfig.s.sol";
+import {DSCEngine} from "../../src/DSCEngine.sol";
+import {DecentralizedStableCoin} from "../../src/DecentralizedStableCoin.sol";
+
+contract DSCEngineTest is Test {
+
+    function setUp() public {
+
+    }
+}
